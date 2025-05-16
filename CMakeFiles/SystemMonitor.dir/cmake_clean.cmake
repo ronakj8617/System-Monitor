e3ef1388203923ecf9d8ca6ef9069c2189d3c83b
Ronak Parmar <ronakp447450@gmail.com>
2025-05-17 00:08:07 +0530

    Bug fixes

---
+++ CMakeFiles/SystemMonitor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SystemMonitor.dir/src/MonitorUI.cpp.o"
+  "CMakeFiles/SystemMonitor.dir/src/MonitorUI.cpp.o.d"
+  "CMakeFiles/SystemMonitor.dir/src/main.cpp.o"
+  "CMakeFiles/SystemMonitor.dir/src/main.cpp.o.d"
+  "SystemMonitor"
+  "SystemMonitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SystemMonitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
